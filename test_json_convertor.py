e380f09927bc65d73087b2ca9de59520a328f582
Jin Zhu <zj7229121@gmail.com>
2022-03-31 15:50:58 +1300

    Rename two test functions' name make them more readable

--- test_json_convertor.py
+++ test_json_convertor.py
@@ -22,12 +22,12 @@ def test_take_in_the_file_exists_with_incorrect_json_format():
     result = json_convertor.read_and_load("test_file_incorrect_json_format",test_list)
     assert result == False
 
-def test_take_in_a_list_with_same_format_with_and_the_file_exists_with_correct_json_format():
+def test_take_in_a_list_with_same_format_and_the_file_exists_with_correct_json_format():
     test_list = [{"name": "It takes two", "required storage": 70}, {"name": "God of War", "required storage": 70}]
     result = json_convertor.read_and_load("test_file_correct_json_format.txt",test_list)
     assert result == True
 
-def test_take_in_a_list_with_different_format_with_and_the_file_exists_with_correct_json_format():
+def test_take_in_a_list_with_different_format_and_the_file_exists_with_correct_json_format():
     test_list = ["apple","orange","banana"]
     result = json_convertor.read_and_load("test_file_correct_json_format.txt",test_list)
     assert result == True
